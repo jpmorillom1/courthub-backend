b13080c0182dade149c2d8976b2bf467d061b305
jpmorillom1 <morillojuanpablo2003@gmail.com>
2026-01-08 01:10:24 -0500

    chore(monorepo): initialize project structure, gradle wrapper and docker config

---
+++ init-databases.sql
@@ -0,0 +1,8 @@
+
+CREATE DATABASE courthub_auth;
+
+CREATE DATABASE courthub_user;
+
+GRANT ALL PRIVILEGES ON DATABASE courthub_auth TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE courthub_user TO postgres;
+
