d2c7d25364db79289a24f78551ae4a44d64da5c4
jpmorillom1 <morillojuanpablo2003@gmail.com>
2026-01-12 18:29:41 -0500

    chore(court-service): configure Docker Compose for court service, init-database and add application properties

--- init-databases.sql
+++ init-databases.sql
@@ -3,6 +3,9 @@ CREATE DATABASE courthub_auth;
 
 CREATE DATABASE courthub_user;
 
+CREATE DATABASE courthub_court;
+
 GRANT ALL PRIVILEGES ON DATABASE courthub_auth TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE courthub_user TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE courthub_court TO postgres;
 
