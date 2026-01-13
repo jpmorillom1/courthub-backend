005cb96868bd10f8fd7a7cbf855bbbf783c0d76a
jpmorillom1 <morillojuanpablo2003@gmail.com>
2026-01-13 16:42:55 -0500

    feat(global-config): add Booking Service to Docker Compose and initialize database

--- init-databases.sql
+++ init-databases.sql
@@ -5,7 +5,10 @@ CREATE DATABASE courthub_user;
 
 CREATE DATABASE courthub_court;
 
+CREATE DATABASE courthub_booking;
+
 GRANT ALL PRIVILEGES ON DATABASE courthub_auth TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE courthub_user TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE courthub_court TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE courthub_booking TO postgres;
 
